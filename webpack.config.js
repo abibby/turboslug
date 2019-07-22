65c62f7f441ec3bf46455d65ec203d6acbc66bf3
Adam Bibby <adam_bibby@hotmail.com>
2019-07-22 10:00:39 -0400

    updated webpack

--- webpack.config.js
+++ webpack.config.js
@@ -8,72 +8,75 @@ const paths = {
     dist: path.resolve(__dirname, 'dist'),
 }
 
-const devMode = true
+module.exports = (env, argv) => {
+    const devMode = argv.mode !== 'production'
 
-module.exports = {
-    entry: path.join(paths.js, 'index.tsx'),
-    mode: devMode ? 'development' : 'production',
-    devtool: devMode ? 'source-map' : '',
-    module: {
-        rules: [
-            {
-                test: /\.tsx?$/,
-                exclude: /node_modules/,
-                loader: [
-                    'ts-loader',
-                    'tslint-loader',
-                ],
-            },
-            {
-                test: /\.scss$/,
-                use: [
-                    MiniCssExtractPlugin.loader,
-                    'css-loader',
-                    {
-                        loader: 'postcss-loader',
-                        options: {
-                            ident: 'postcss',
-                            plugins: [
-                                require("css-mqpacker")({ sort: true }),
-                                require('autoprefixer'),
-                            ]
+    return {
+        entry: path.join(paths.js, 'index.tsx'),
+        mode: devMode ? 'development' : 'production',
+        devtool: devMode ? 'source-map' : '',
+        module: {
+            rules: [{
+                    test: /\.tsx?$/,
+                    exclude: /node_modules/,
+                    loader: [
+                        'ts-loader',
+                        'tslint-loader',
+                    ],
+                },
+                {
+                    test: /\.scss$/,
+                    use: [
+                        MiniCssExtractPlugin.loader,
+                        'css-loader',
+                        {
+                            loader: 'postcss-loader',
+                            options: {
+                                ident: 'postcss',
+                                plugins: [
+                                    require("css-mqpacker")({
+                                        sort: true
+                                    }),
+                                    require('autoprefixer'),
+                                ]
+                            },
                         },
-                    },
-                    {
-                        loader: "sass-loader",
-                        options: {
-                            sourceMap: true,
-                            includePaths: [
-                                'node_modules', 'src', '.'
-                            ]
-                        }
-                    },
-                ],
-            },
-        ]
-    },
-    output: {
-        path: paths.dist,
-        filename: './' + (devMode ? '[name].js' : '[name].[hash].js'),
-        chunkFilename: './' + (devMode ? '[id].js' : '[id].[hash].js'),
-    },
-    resolve: {
-        extensions: ['.tsx', '.ts', '.js', '.scss', '.css'],
-        modules: [
-            "node_modules",
-            paths.src,
+                        {
+                            loader: "sass-loader",
+                            options: {
+                                sourceMap: true,
+                                includePaths: [
+                                    'node_modules', 'src', '.'
+                                ]
+                            }
+                        },
+                    ],
+                },
+            ]
+        },
+        output: {
+            path: paths.dist,
+            filename: './' + (devMode ? '[name].js' : '[name].[hash].js'),
+            chunkFilename: './' + (devMode ? '[id].js' : '[id].[hash].js'),
+        },
+        resolve: {
+            extensions: ['.tsx', '.ts', '.js', '.scss', '.css'],
+            modules: [
+                "node_modules",
+                paths.src,
+            ],
+        },
+        plugins: [
+            new HtmlWebpackPlugin({
+                template: path.join(paths.src, 'index.html'),
+                filename: "index.html",
+            }),
+            new MiniCssExtractPlugin({
+                // Options similar to the same options in webpackOptions.output
+                // both options are optional
+                filename: './' + (devMode ? '[name].css' : '[name].[hash].css'),
+                chunkFilename: './' + (devMode ? '[id].css' : '[id].[hash].css'),
+            }),
         ],
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: path.join(paths.src, 'index.html'),
-            filename: "index.html",
-        }),
-        new MiniCssExtractPlugin({
-            // Options similar to the same options in webpackOptions.output
-            // both options are optional
-            filename: './' + (devMode ? '[name].css' : '[name].[hash].css'),
-            chunkFilename: './' + (devMode ? '[id].css' : '[id].[hash].css'),
-        }),
-    ],
-};
+    }
+}
