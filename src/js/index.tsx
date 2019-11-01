7df1f7f13b496058d2f8e18db0bb2923716275b6
Adam Bibby <adam_bibby@hotmail.com>
2019-11-01 09:03:34 -0400

    readd service worker

--- src/js/index.tsx
+++ src/js/index.tsx
@@ -48,6 +48,6 @@ render(<Index />, document.getElementById('app')!)
 if ('serviceWorker' in navigator) {
     // Use the window load event to keep the page load performant
     window.addEventListener('load', () => {
-        // navigator.serviceWorker.register('/service-worker.js')
+        navigator.serviceWorker.register('/service-worker.js')
     })
 }
