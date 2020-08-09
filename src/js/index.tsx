e282d3e22d2fdd9a519b2750a4d8fd7281112b8d
Adam Bibby <Adam_Bibby@hotmail.com>
2020-08-09 08:25:36 -0400

    removed service worker (#45)

--- src/js/index.tsx
+++ src/js/index.tsx
@@ -44,10 +44,10 @@ class Index extends Component<{}, State> {
 
 render(<Index />, document.getElementById('app')!)
 
-// Check that service workers are supported
-if ('serviceWorker' in navigator) {
-    // Use the window load event to keep the page load performant
-    window.addEventListener('load', () => {
-        navigator.serviceWorker.register('/service-worker.js')
-    })
-}
+// // Check that service workers are supported
+// if ('serviceWorker' in navigator) {
+//     // Use the window load event to keep the page load performant
+//     window.addEventListener('load', () => {
+//         navigator.serviceWorker.register('/service-worker.js')
+//     })
+// }
